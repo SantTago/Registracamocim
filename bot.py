c68a9d3ba887a4ab44e9ccaf998aa038300dbec8
TIAGO  VIANA DA CRUZ <mocobom09@gmail.com>
2025-02-28 23:56:10 -0300

    img

--- bot.py
+++ bot.py
@@ -7,49 +7,168 @@ time.sleep(3)
 
 # Loop infinito até pressionar 'Espaço'
 while True:
-    # Se a tecla 'espaço' for pressionada, o loop para
     if keyboard.is_pressed('space'):
         print("\nExecução interrompida pelo usuário! 🚀")
         break
 
     # Primeiro clique (Botão Direito)
-    x1, y1 = 742, 306
+    x1, y1 = 863, 502
     pyautogui.click(x=x1, y=y1, button='right')
     print(f"1º Clique (Direito) em X={x1}, Y={y1}")
 
-    time.sleep(3)  # Pausa entre cliques
+    time.sleep(2)  # Pausa entre cliques
 
-    # Segundo clique (Botão Esquerdo)
-    x2, y2 = 782, 368
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 953, 395
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    x2, y2 = 953, 395
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(3)
+
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 508, 445
     pyautogui.click(x=x2, y=y2, button='left')
     print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
 
     time.sleep(2)
 
-     # Segundo clique (Botão Esquerdo)
-    x2, y2 = 782, 368
+    #apaga
+    x1, y1 = 863, 502
+    pyautogui.click(x=x1, y=y1, button='right')
+    print(f"1º Clique (Direito) em X={x1}, Y={y1}")
+
+    time.sleep(2)  # Pausa entre cliques
+ 
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 916, 480
     pyautogui.click(x=x2, y=y2, button='left')
     print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
 
-    time.sleep(5)
+    time.sleep(2)
 
-    
+    x2, y2 = 916, 480
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
 
-    # Terceiro clique (Botão Esquerdo)    
-    x3, y3 = 521, 444
-    pyautogui.click(x=x3, y=y3, button='left')
-    print(f"3º Clique (Esquerdo) em X={x3}, Y={y3}")
+    time.sleep(2)
+
+    x2, y2 = 575, 418
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    #--------------------------------------------------
+
+    x1, y1 = 761, 504
+    pyautogui.click(x=x1, y=y1, button='right')
+    print(f"1º Clique (Direito) em X={x1}, Y={y1}")
+
+    time.sleep(2)  # Pausa entre cliques
+
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 829, 399
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    x2, y2 = 829, 399
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
 
     time.sleep(3)
 
-    # Quarto clique (Botão Esquerdo)
-    x4, y4 = 1330, 375
-    pyautogui.click(x=x4, y=y4, button='left')
-    print(f"4º Clique (Esquerdo) em X={x4}, Y={y4}")
+    x2, y2 = 508, 444
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
 
-    
+    time.sleep(3)
 
-    print("Todos os cliques realizados! Reiniciando... Pressione 'Espaço' para parar.")
-    time.sleep(3)  # Pequena pausa antes de reiniciar c o loop
+    #apaga
+    x1, y1 = 761, 504
+    pyautogui.click(x=x1, y=y1, button='right')
+    print(f"1º Clique (Direito) em X={x1}, Y={y1}")
 
- 
+    time.sleep(2)  # Pausa entre cliques
+
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 815, 475
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    x2, y2 = 815, 475
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    x2, y2 = 575, 418
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    #--------------------------------------------------
+
+
+    x1, y1 = 659, 507
+    pyautogui.click(x=x1, y=y1, button='right')
+    print(f"1º Clique (Direito) em X={x1}, Y={y1}")
+
+    time.sleep(2)  # Pausa entre cliques
+
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 719, 401
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    x2, y2 = 719, 401
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(3)
+
+    x2, y2 = 508, 444
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(3)
+
+    #apaga
+    x1, y1 = 660, 503
+    pyautogui.click(x=x1, y=y1, button='right')
+    print(f"1º Clique (Direito) em X={x1}, Y={y1}")
+
+    time.sleep(2)  # Pausa entre cliques
+
+        # Segundo clique (Botão Esquerdo)
+    x2, y2 = 740, 481
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    x2, y2 = 740, 481
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    x2, y2 = 535, 417
+    pyautogui.click(x=x2, y=y2, button='left')
+    print(f"2º Clique (Esquerdo) em X={x2}, Y={y2}")
+
+    time.sleep(2)
+
+    #--------------------------------------------------
+
+
+
+print("Todos os cliques realizados! Reiniciando... Pressione 'Espaço' para parar.")
+time.sleep(3)  # Pequena pausa antes de reiniciar c o loop
+
