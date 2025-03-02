1bad3169c866fc4794af6c82f6e205dde44e2ebe
TIAGO  VIANA DA CRUZ <mocobom09@gmail.com>
2025-03-02 20:11:31 -0300

    imgs

--- redimencionarimgs.py
+++ redimencionarimgs.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-def resize_images(input_folder, height=800):
+def resize_images(input_folder, height=600):
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(('png', 'jpg', 'jpeg', 'webp')):
             img_path = os.path.join(input_folder, filename)
@@ -16,7 +16,7 @@ def resize_images(input_folder, height=800):
             print(f"Imagem redimensionada: {img_path}")
 
 # Caminho da pasta de imagens
-input_folder = "imagens/@raquelsiqueira"
+input_folder = "imagens/@Zehostilio96"
 
 # Chamar a função para redimensionar as imagens mantendo a proporção
 resize_images(input_folder)
